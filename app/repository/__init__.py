f535ff5f0fad6828c51e0691cf6b04bf2c2d2764
azizext <aziz.rehman@nets-international.com>
2024-01-04 12:33:33 +0500

    initial version

--- app/repository/__init__.py
+++ app/repository/__init__.py
@@ -1,5 +1,3 @@
-# from app.repository.post_repository import PostRepository
-# from app.repository.tag_repository import TagRepository
 from app.repository.user_repository import UserRepository
 from app.repository.site_repository import SiteRepository
 from app.repository.rack_repository import RackRepository
