2d2dd51c9c96613b8a9fd64b17f006b4a785e1b7
azizext <aziz.rehman@nets-international.com>
2024-01-12 15:47:04 +0500

    12 jan

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -6,3 +6,4 @@ class UserService(BaseService):
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
         super().__init__(user_repository)
+c
