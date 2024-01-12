a9da2ab7548d8fdfa73b75a8cca102529c872ba2
azizext <aziz.rehman@nets-international.com>
2024-01-12 16:56:53 +0500

    updated with login api

--- FAST_BACKEND/app/services/user_service.py
+++ FAST_BACKEND/app/services/user_service.py
@@ -6,4 +6,3 @@ class UserService(BaseService):
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
         super().__init__(user_repository)
-c
