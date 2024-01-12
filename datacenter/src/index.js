defa5506cd1e2dd0169a2a4994a87b53d539a2f2
shahmasood-Extravis <shah.masood@nets-international.com>
2024-01-12 12:46:10 +0500

    ...

--- datacenter/src/index.js
+++ datacenter/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import AppContextProvider from "./context/appContext"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AppContextProvider from "./context/appContext";
+// import AdminDashboard from "./containers/dashboardModule/adminDashboard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-<AppContextProvider>
-    <App />
-
+    <AppContextProvider>
+      <App />
+      {/* <AdminDashboard /> */}
     </AppContextProvider>
   </React.StrictMode>
 );
