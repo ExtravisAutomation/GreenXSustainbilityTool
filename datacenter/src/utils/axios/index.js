2eef8bed597eef52dd6eb83b5bdd5494bdfd58ab
shahmasood-Extravis <shah.masood@nets-international.com>
2024-01-17 11:37:37 +0500

    worked on axios file

--- datacenter/src/utils/axios/index.js
+++ datacenter/src/utils/axios/index.js
@@ -1,2 +1,2 @@
 // export const baseUrl = "http://localhost:8000/api/v2/auth";
-export const baseUrl = "http://13.43.212.6:3010/api/v2/auth";
+export const baseUrl = "http://13.43.212.6:8000/api/v2/auth";
