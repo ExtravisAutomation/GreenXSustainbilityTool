bdbfca53b63e04516a6bf0cb8fabfcbdf0541f4c
alihassan2000 <102433608+AliHassan2000@users.noreply.github.com>
2024-01-29 17:13:07 +0500

    Update index.js

--- datacenter/src/utils/axios/index.js
+++ datacenter/src/utils/axios/index.js
@@ -1,2 +1,2 @@
 // export const baseUrl = "http://localhost:8000/api/v2/auth";
-export const baseUrl = "http://13.43.212.6:8000/api/v2/auth";
+export const baseUrl = "http://13.43.212.6:5010/api/v2/auth";
