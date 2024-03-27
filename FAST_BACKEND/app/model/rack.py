a1602b0e9972298e22abaf5e660611c244fa5dcc
azizext <aziz.rehman@nets-international.com>
2024-03-27 12:06:08 +0500

    updatedkpifilter

--- FAST_BACKEND/app/model/rack.py
+++ FAST_BACKEND/app/model/rack.py
@@ -1,26 +1,26 @@
 from sqlalchemy import Column, String, Integer, Date, ForeignKey
 from sqlalchemy.orm import relationship
-from app.model.base_model import BaseModel
+from app.model.base_model import BaseModel  # Assuming BaseModel includes id and timestamp fields
 
 
 class Rack(BaseModel):
     __tablename__ = "rack"
 
     rack_name = Column(String(255), nullable=False)
-    site_id = Column(Integer, ForeignKey('site.id'), nullabe=False)
+    site_id = Column(Integer, ForeignKey('site.id'), nullable=False)
     manufacture_date = Column(Date, nullable=True)
-    unit_position = Column(Integer, nullable=True)  # change to string
+    unit_position = Column(Integer, nullable=True)
     rack_model = Column(String(255), nullable=True)
-    pn_code = Column(String(255), nullable=True)  # remove
+    pn_code = Column(String(255), nullable=True)
     serial_number = Column(String(255), nullable=True)
-    Ru = Column(Integer, nullable=True)  # change to no of ru
-    RFS = Column(String(255), nullable=True)  # remove
-    Height = Column(Integer, nullable=True)  # change to float
-    Width = Column(Integer, nullable=True)  # change to float
-    Depth = Column(Integer, nullable=True)  # change to float
+    Ru = Column(Integer, nullable=True)
+    RFS = Column(String(255), nullable=True)
+    Height = Column(Integer, nullable=True)
+    Width = Column(Integer, nullable=True)
+    Depth = Column(Integer, nullable=True)
     Tag_id = Column(String(255), nullable=True)
-    floor = Column(Integer, nullable=True)  # string
+    floor = Column(Integer, nullable=True)
     status = Column(String(255), nullable=True)
-    total_devices = Column(Integer, nullable=True)  # remove
+    total_devices = Column(Integer, nullable=True)
     site = relationship("Site", back_populates="racks")
     devices = relationship("APICControllers", back_populates="rack")
