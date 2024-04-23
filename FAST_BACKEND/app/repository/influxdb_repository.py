fa343e19b63a59843e6beabc949d083f93a37457
azizext <aziz.rehman@nets-international.com>
2024-04-23 18:47:22 +0500

    dsadas

--- FAST_BACKEND/app/repository/influxdb_repository.py
+++ FAST_BACKEND/app/repository/influxdb_repository.py
@@ -1303,11 +1303,11 @@ class InfluxDBRepository:
             if result.empty:
                 print(f"No data found for {ip}, generating dummy data")
                 dummy_data = self.generate_dummy_data12(exact_time, granularity)
-                filtered_metrics.extend(dummy_data)
+                filtered_metrics.append(dummy_data)
                 print("DUMMYMETRICCCCCCCCCCCCBEFOREEEEEE", filtered_metrics, file=sys.stderr)
                 print(f"Dummy data generated for {ip}: {dummy_data}")
                 print("DUMMYMETRICCCCCCCCCCCCAFTERRRRRRR", filtered_metrics, file=sys.stderr)
-                break
+
 
             else:
                 print(f"Data found for {ip}, processing results")
