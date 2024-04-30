16cd1c4ebdcba1875dc95d38db5738430d214bdd
azizext <aziz.rehman@nets-international.com>
2024-04-30 14:27:33 +0500

    dasdasdas

--- FAST_BACKEND/app/services/auth_service.py
+++ FAST_BACKEND/app/services/auth_service.py
@@ -30,28 +30,45 @@ class AuthService(BaseService):
     def sign_in(self, sign_in_info: SignInNew):
         find_user = FindUser()
         find_user.user_name = sign_in_info.user_name
+        print(f"Attempting to find user with username: {find_user.user_name}", file=sys.stderr)
+
         user: User = self.user_repository.read_by_options(find_user)["found"]
-        print("USER", user, file=sys.stderr)
+        print(f"User found: {user.name}", file=sys.stderr)
+
         if user is None:
+            print("Authentication error: Incorrect username or password", file=sys.stderr)
             raise AuthError(detail="Incorrect username or password")
+
         if not user.is_active:
+            print(f"Account activity error: Account for {user.user_name} is not active", file=sys.stderr)
             raise AuthError(detail="Account is not active")
+
         if not verify_password(sign_in_info.password, user.password):
+            print("Authentication error: Password does not match", file=sys.stderr)
             raise AuthError(detail="Incorrect username or password")
+
+        print("Password verified successfully", file=sys.stderr)
         delattr(user, "password")
+
         payload = Payload(
             id=user.id,
             email=user.email,
             name=user.name,
             is_superuser=user.is_superuser,
         )
+        print(f"Payload prepared for user {user.id}: {payload}", file=sys.stderr)
+
         token_lifespan = timedelta(minutes=configs.ACCESS_TOKEN_EXPIRE_MINUTES)
         access_token, expiration_datetime = create_access_token(payload.dict(), token_lifespan)
+        print(f"Access token created: {access_token}", file=sys.stderr)
+
         sign_in_result = {
             "access_token": access_token,
             "expiration": expiration_datetime,
             "user_info": user,
         }
+        print(f"Sign-in result prepared: {sign_in_result}", file=sys.stderr)
+
         return sign_in_result
 
     def sign_up(self, user_info: SignUp):
