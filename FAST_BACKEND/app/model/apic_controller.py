b00eb4f27e85e098a3009fe6c011a6d24b778873
azizext <aziz.rehman@nets-international.com>
2024-07-01 13:28:50 +0500

    dsadasd

--- FAST_BACKEND/app/model/apic_controller.py
+++ FAST_BACKEND/app/model/apic_controller.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import relationship
 from .base_model import BaseModel
 
 
-# class APICController(BaseModel):
-#     __tablename__ = "apic_controllers"
-#
-#     ip_address = Column(String(255), unique=False, index=True)
-#     fabric_nodes = relationship("FabricNode", back_populates="apic_controller")
-#     deviceInventory = relationship("DeviceInventory", back_populates="apic_controller")
-
 class APICController(BaseModel):
-    __tablename__ = 'apic_controllers'
+    __tablename__ = "apic_controllers"
 
-    ip_address = Column(String(255), nullable=False)
+    ip_address = Column(String(255), unique=False, index=True)
+    fabric_nodes = relationship("FabricNode", back_populates="apic_controller")
+    deviceInventory = relationship("DeviceInventory", back_populates="apic_controller")
+
+# class APICController(BaseModel):
+#     __tablename__ = 'apic_controllers'
+#
+#     ip_address = Column(String(255), nullable=False)
