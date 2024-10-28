79f9824f063b2618e1b7b7c2244e959dcaf4a7d0
azizext <aziz.rehman@nets-international.com>
2024-10-28 13:02:03 +0500

    133333333333333

--- FAST_BACKEND/app/services/device_inventory_service.py
+++ FAST_BACKEND/app/services/device_inventory_service.py
@@ -16,7 +16,43 @@ class DeviceInventoryService:
         enriched_devices = []
         for device in devices:
             enriched_device = DeviceInventoryInDB(
-                **device.__dict__,
+                # Set only required fields explicitly
+                cisco_domain=device.cisco_domain,
+                contract_expiry=device.contract_expiry,
+                contract_number=device.contract_number,
+                created_by=device.created_by,
+                criticality=device.criticality,
+                department=device.department,
+                device_id=device.device_id,
+                device_name=device.device_name,
+                device_ru=device.device_ru,
+                domain=device.domain,
+                hardware_version=device.hardware_version,
+                hw_eol_date=device.hw_eol_date,
+                hw_eos_date=device.hw_eos_date,
+                item_code=device.item_code,
+                item_desc=device.item_desc,
+                manufacturer_date=device.manufacturer_date,
+                manufacturer=device.manufacturer,
+                modified_by=device.modified_by,
+                parent=device.parent,
+                patch_version=device.patch_version,
+                pn_code=device.pn_code,
+                site_id=device.site_id,
+                rack_id=device.rack_id,
+                rfs_date=device.rfs_date,
+                section=device.section,
+                serial_number=device.serial_number,
+                software_version=device.software_version,
+                source=device.source,
+                stack=device.stack,
+                status=device.status,
+                sw_eol_date=device.sw_eol_date,
+                sw_eos_date=device.sw_eos_date,
+                tag_id=device.tag_id,
+                apic_controller_id=device.apic_controller_id,
+
+                # Convert SNTC fields
                 hw_eol_ad=self.to_datetime(device.hw_eol_ad),
                 hw_eos=self.to_datetime(device.hw_eos),
                 sw_EoSWM=self.to_datetime(device.sw_EoSWM),
@@ -24,6 +60,8 @@ class DeviceInventoryService:
                 sw_EoVSS=self.to_datetime(device.sw_EoVSS),
                 hw_EoSCR=self.to_datetime(device.hw_EoSCR),
                 hw_ldos=self.to_datetime(device.hw_ldos),
+
+                # Additional fields
                 site_name=device.site.site_name if device.site else None,
                 rack_name=device.rack.rack_name if device.rack else None,
                 device_ip=device.apic_controller.ip_address if device.apic_controller else None
