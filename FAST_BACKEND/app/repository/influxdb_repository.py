3fcda15ab9a0f66ce487130105665a11d49342db
azizext <aziz.rehman@nets-international.com>
2024-10-29 14:41:05 +0500

    eerdetails

--- FAST_BACKEND/app/repository/influxdb_repository.py
+++ FAST_BACKEND/app/repository/influxdb_repository.py
@@ -2791,8 +2791,8 @@ class InfluxDBRepository:
                         total_power_metrics.append({
                             "time": row['index'],
                             "energy_consumption": round(energy_consumption, 2),
-                            "total_POut": round(pout, 2),
-                            "total_PIn": round(pin, 2),
+                            "total_POut": round(pout/1000, 2),
+                            "total_PIn": round(pin/1000, 2),
                             "power_efficiency": round(power_efficiency, 2),
                             "eer": round(eer, 2),
                             "pue": round(pue, 2)
