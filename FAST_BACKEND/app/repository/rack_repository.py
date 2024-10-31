6459950c94d4e49c973bb4b613b653c20016025e
azizext <aziz.rehman@nets-international.com>
2024-10-31 15:55:42 +0500

    building22

--- FAST_BACKEND/app/repository/rack_repository.py
+++ FAST_BACKEND/app/repository/rack_repository.py
@@ -22,6 +22,8 @@ from app.model.rack import rack_building_association
 
 from app.schema.rack_schema import RackDetails
 
+from app.schema.building_schema import BuildingCreate
+
 
 class RackRepository(BaseRepository):
     def __init__(self, session_factory: Callable[..., AbstractContextManager[Session]]):
