f742bf8e8ed9deff3e2305e6098f5b19bb54fd8f
mutahhar-kaspar <muhammad.mutahhar@1kaspar.com>
2024-11-07 14:58:59 +0500

    basicopenaiapii

--- FAST_BACKEND/app/repository/site_repository.py
+++ FAST_BACKEND/app/repository/site_repository.py
@@ -37,7 +37,7 @@ from app.model.APIC_controllers import APICControllers as Devices
 
 from app.model.cspc_devices import CSPCDevices
 from app.schema.site_schema import CSPCDevicesWithSntcResponse
-import openai
+#import openai
 from app.core.config import configs
 class SiteRepository(BaseRepository):
     def __init__(self, session_factory: Callable[..., AbstractContextManager[Session]]):
@@ -1063,20 +1063,20 @@ class SiteRepository(BaseRepository):
             else:
                 return None
 
-    def get_openai_answer(self, question: str) -> str:
-        try:
-            response = openai.ChatCompletion.create(
-                model="gpt-4o",
-                messages=[
-                    {"role": "system",
-                     "content": "You are a Cisco Devices data analyst and expert, specializing in data analysis."},
-                    {"role": "user", "content": question}
-                ],
-                max_tokens=50
-            )
-            answer = response.choices[0].message["content"]
-            return answer
-        except openai.error.AuthenticationError:
-            raise HTTPException(status_code=401, detail="Invalid or inactive OpenAI API key.")
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Error interacting with OpenAI API: {str(e)}")
+    # def get_openai_answer(self, question: str) -> str:
+    #     try:
+    #         response = openai.ChatCompletion.create(
+    #             model="gpt-4o",
+    #             messages=[
+    #                 {"role": "system",
+    #                  "content": "You are a Cisco Devices data analyst and expert, specializing in data analysis."},
+    #                 {"role": "user", "content": question}
+    #             ],
+    #             max_tokens=50
+    #         )
+    #         answer = response.choices[0].message["content"]
+    #         return answer
+    #     except openai.error.AuthenticationError:
+    #         raise HTTPException(status_code=401, detail="Invalid or inactive OpenAI API key.")
+    #     except Exception as e:
+    #         raise HTTPException(status_code=500, detail=f"Error interacting with OpenAI API: {str(e)}")
