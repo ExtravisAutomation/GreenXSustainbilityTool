cc61bc688c6a8b5387ee77b1043c11db42f9c6cc
samankhawart <saman.khawar@nets-international.com>
2025-03-28 12:09:22 +0500

    prediction tableSSss

--- FAST_BACKEND/app/schema/site_schema.py
+++ FAST_BACKEND/app/schema/site_schema.py
@@ -476,7 +476,7 @@ class EnergyConsumptionMetricsDetailsNew(BaseModel):
 
 class DeviceRequest(BaseModel):
     device_id: int
-    site_id:int
+    site_id:Optional[int] = 0
 
 class modelResponse(BaseModel):
     limit: Optional[int] = 0
