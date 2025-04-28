f865174de2f36011da3dfb2f2774b5688574aeb8
samankhawart <saman.khawar@nets-international.com>
2025-04-28 11:35:07 +0500

    report urls

--- FAST_BACKEND/app/api/v2/endpoints/site.py
+++ FAST_BACKEND/app/api/v2/endpoints/site.py
@@ -1876,7 +1876,7 @@ def view_pdf(
 
     # Secure the filename to avoid directory traversal attacks
     safe_filename = os.path.basename(filename)
-    pdf_path = Path(f"/home/dev/reporting/ega_reports/reports/{safe_filename}")
+    pdf_path = Path(f"/home/dev/reporting/ega_reports/{safe_filename}")
     print("sdakfkdjskdgjk")
 
     if pdf_path.exists():
